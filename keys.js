f200af5bcbb5c3059e55e205dd92426e024bc34e
LilTimmyBynum <Timothy_JA_Bynum@yahoo.com>
2017-06-21 01:16:09 -0700

    adding initial files

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+	
+	consumer_key: '	la725DGM486kBCvzH2R10FVKn',
+	consumer_secret: '	mUPyz782cNEg00KHMhGsnIXUyONhv19acoCIl53SV7As8wcF8v', 
+	access_token_key: '877424587801206784-XU214AmAGU1xbEBWg88mxQr9L3Ou6zA',
+	access_token_secret: '	hwFeswapUTvGkftdHc37dfYGquLS72qWCSpFaCvlXC889',
+}
